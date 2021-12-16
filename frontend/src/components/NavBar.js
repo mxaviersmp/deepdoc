4856c987d4bb92278472fbb01bf01e824441e491
Matheus Xavier Sampaio <matheus.sampaio@petz.com.br>
2021-12-16 10:23:39 -0300

    recreated file

---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+
+  return (
+    <div>
+      <img  src={require('../static/img/logo-deepdoc.png')}
+            alt="logomarca da ferramenta deepdoc"
+            className='logo'></img>
+      <div className='grayBar' />
+    </div>
+  )
+}
+
+export default Navbar
